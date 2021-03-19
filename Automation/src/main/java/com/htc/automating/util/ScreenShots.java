0e5f77c4245deb3508034390ecb3a9d2662d08eb
SAI SRINIVAS <SAI SRINIVAS@DESKTOP-H7SUIF6>
2021-03-19 16:01:34 +0530

    this is the fourth step

--- Automation/src/main/java/com/htc/automating/util/ScreenShots.java
+++ Automation/src/main/java/com/htc/automating/util/ScreenShots.java
@@ -16,42 +16,49 @@ import com.htc.reports.ExtentTestReport;
 
 public class ScreenShots {
 	private WebDriver driver;
+	TakesScreenshot tss;
+	File srcfile,destfile;
+	String timestamp= new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(new Date());
 	
 	public ScreenShots(WebDriver driver) {
 		this.driver=driver;
 	}
 	
-	public void capturingScreenShot() {		
-		String timestamp= new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(new Date());
-		TakesScreenshot tss=(TakesScreenshot)driver;
-		File srcfile=tss.getScreenshotAs(OutputType.FILE);
-		File destfile= new File(GlobalVariables.SCREENSHOT_PATH+"Test "+timestamp+".png");
+	public void capturingScreenShot() throws NullPointerException {		
 		try {
+			tss=(TakesScreenshot)driver;
+			srcfile=tss.getScreenshotAs(OutputType.FILE);
+			destfile= new File(GlobalVariables.SCREENSHOT_PATH+"Test "+timestamp+".png");
 			Thread.sleep(5000);
 			FileUtils.copyFile(srcfile, destfile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void addingScreenshotToReport() {
-		String timestamp= new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(new Date());
-		TakesScreenshot tss=(TakesScreenshot)driver;
-		File srcfile=tss.getScreenshotAs(OutputType.FILE);
-		File destfile= new File(GlobalVariables.REPORT_SCREENSHOT_PATH+"Test "+timestamp+".png");
+	public void addingScreenshotToReport() throws NullPointerException{
 		try {
+			tss=(TakesScreenshot)driver;
+			srcfile=tss.getScreenshotAs(OutputType.FILE);
+			destfile= new File(GlobalVariables.REPORT_SCREENSHOT_PATH+"Test "+timestamp+".png");
 			Thread.sleep(5000);
 			FileUtils.copyFile(srcfile, destfile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		try {
 			ExtentTestReport.getTest().fail("Screenshot",MediaEntityBuilder.createScreenCaptureFromPath(GlobalVariables.HTML_REPORT_PATH).build());
 		} catch (IOException e) {
+			e.printStackTrace();
+		}catch (Exception e) {
 			e.printStackTrace();
 		}		
 	}
